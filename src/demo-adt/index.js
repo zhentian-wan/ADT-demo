e551296c526841b36bd4a4ba9388776a2daf2f0f
zhentian-wan <answer881215@gmail.com>
2019-03-16 14:07:27 +0200

    state moand

--- src/demo-adt/index.js
+++ src/demo-adt/index.js
@@ -1,22 +1,37 @@
 const log = require('./lib/log');
-const Pair = require('crocks/Pair');
-const State = require('crocks/State');
 
+const State = require('crocks/State');
+const B = require('crocks/combinators/composeB');
+const K = require('crocks/combinators/constant');
+const prop = require('crocks/Maybe/prop');
+const option = require('crocks/pointfree/option');
+const {modify, get} = State;
 const assign = require('crocks/helpers/assign');
 
-const {deck, displayCards, shuffleCards} = require('./model/deck');
+// pluckSeed :: Integer -> Object -> Integer
+const pluckSeed =
+    def => B(option(def), prop('seed'))
 
- // State s a
+// rando : Integer -> State Object Float
+const rando = x => {
+    const seed = (1103515245 * x + 12345) & 0x7fffffff
+    const value = (seed >>> 16) / 0x7fff;
 
- const rando = () => State(s => {
-     const seed = (1103515245 * s.seed + 12345) & 0x7ffffff;
-     const value = (seed >>> 16) / 0x7ff;
+    return modify(assign({seed})) // update the seed Pair(_, seed)
+        .map(K(value)); // update the value Pair(value, seed)
+}
 
-     return Pair(value, assign({seed}, s))
- })
+// pullRandom :: Integer -> State Object Float
+const pullRandom =
+    defSeed => get(s => console.log(s)||pluckSeed(defSeed)(s)).chain(rando);
 
+// limitIndx :: Integer -> Float -> Integer
+const limitIndx = len => x => (x * len) | 0;
 
-
+const seed = 76;
 log(
-    rando().runWith({seed: 10})
+    State.of(seed)
+    .chain(pullRandom) // 'Pair( 0.13864558854945525, { seed: 297746555 } )'
+    .map(limitIndx(52)) // 'Pair( 7, { seed: 297746555 } )'
+    .runWith({seed: 10})
 );

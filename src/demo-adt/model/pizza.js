0c332863b8c56fb0d76aa7b212b0bb0d5ff0c656
zhentian-wan <answer881215@gmail.com>
2019-03-24 16:31:45 +0200

    quick save

--- src/demo-adt/model/pizza.js
+++ src/demo-adt/model/pizza.js
@@ -1,12 +1,14 @@
 const log = require('../lib/log');
 
 const Arrow = require('crocks/Arrow');
+const branch = require('crocks/Pair/branch');
 const chain = require('crocks/pointfree/chain');
 const compose = require('crocks/helpers/compose');
 const composeS = require('crocks/helpers/composeS');
 const concat = require('crocks/pointfree/concat');
 const isString = require('crocks/predicates/isString');
 const map = require('crocks/pointfree/map');
+const merge = require('crocks/pointfree/merge');
 const objOf = require('crocks/helpers/objOf');
 const option = require('crocks/pointfree/option');
 const prop = require('crocks/Maybe/prop');
@@ -33,34 +35,22 @@ const cheesePizza = composeS(
 );
 
 // cookPizza : {raw: a} -> {cooked: b}
-const cookPizza = compose(
-    option({}),
+const cookPizza = def => compose(
     map(objOf('cooked')),
     prop('raw')
-);
+)(def);
+
+const package = x => [x];
+
 // contramap : (a -> c)-> f c b -> f a b
 // Arrow : [] --contramap--> {raw: []} -> {cooked: []}
 const oven =
     Arrow(cookPizza)
-    .contramap(objOf('raw'))
+    .second() // 'Pair( { raw: "teeth" }, Just { cooked: "teeth" } )'
+    .contramap(branch)
+    .map(merge(option)) // { cooked: "teeth" }
 
 log(
-    cheesePizza
-        .compose(oven)
-        .runWith([])
+    oven
+        .runWith({raw: 'teeth'})
 )
-
-const getName = compose(
-    option('no name'),
-    chain(safe(isString)),
-    prop('name')
-)
-const arrUpper = Arrow(
-    str => str.toUpperCase()
-)
-const nameUpper = arrUpper
-    .contramap(getName)
-
-log(
-    nameUpper.runWith('zhentian')
-) // ZHENTIAN

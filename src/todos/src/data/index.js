ed3d10c04cf4daa78e68666968b40e9fd3938796
zhentian-wan <answer881215@gmail.com>
2019-04-07 20:49:59 +0300

    Query

---
+++ src/todos/src/data/index.js
@@ -0,0 +1,44 @@
+const {Pred, chain, compose, equals,
+       mconcatMap, merge, option, or,
+       prop, safe, isFunction, isSameType,
+       toPairs, unless} = require('crocks');
+
+const predOrEq = unless(
+    or(isFunction, isSameType(Pred)), // When it is not a function or not a Pred monoid
+    equals // then just check whether values are equal.
+)
+
+// propSatisfies :: (String, a -> boolean) -> Object -> Boolean
+const propSatisfies = (key, pred) => compose(
+    option(false),
+    chain(safe(predOrEq(pred))),
+    prop(key)
+)
+
+// where :: Object -> Object -> Boolean
+const where = compose(
+    mconcatMap(Pred, merge(propSatisfies)),
+    toPairs
+)
+
+const test = where({
+    id: 32,
+    name: 'Joey'
+})
+
+const test2 = where({
+    id: x => x > 40,
+    name: equals('Wan')
+})
+
+console.log(
+    test.runWith({
+        id: 32, name: 'Joey'
+    })
+) // true
+
+console.log(
+    test2.runWith({
+        id: 12, name: 'Wan'
+    })
+) // false

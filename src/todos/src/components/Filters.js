66efd729eb9830bf4c3c2118b06af185ae18dde6
zhentian-wan <answer881215@gmail.com>
2019-04-11 22:14:44 +0300

    Pair merge JSX

--- src/todos/src/components/Filters.js
+++ src/todos/src/components/Filters.js
@@ -3,15 +3,30 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import FilterLink from './FilterLink'
 
+import Pair from 'crocks/Pair'
+import map from 'crocks/pointfree/map'
+import merge from 'crocks/pointfree/merge'
+
 import {filters} from '../data/reducers/filterTodos'
 
 const {SHOW_ALL, SHOW_ACTIVE, SHOW_COMPLETED} = filters;
 
-const Filters = ({todoFilter}) => (
+const filterList = [
+    Pair(SHOW_ALL, 'All'),
+    Pair(SHOW_COMPLETED, 'Completed'),
+    Pair(SHOW_ACTIVE, 'Active')
+]
+
+// buildFilter :: (String, String) -> JSX
+const buildFilter = (filter, text) =>
+    <FilterLink key={filter} filter={filter}>{text}</FilterLink>
+
+const buildFitlers =
+    map(merge(buildFilter))
+
+const Filters = () => (
     <div className="filters">
-        <FilterLink filter={SHOW_ALL} active={todoFilter === SHOW_ALL}>All</FilterLink>
-        <FilterLink filter={SHOW_COMPLETED}>Completed</FilterLink>
-        <FilterLink filter={SHOW_ACTIVE}>Active</FilterLink>
+        {buildFitlers(filterList)}
     </div>
 )
 
